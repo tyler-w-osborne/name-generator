5183a396d17a01fe1ed1f269ba22a7b51e1a4c65
tyler osborne <tylerosborne2018@hotmail.com>
2024-10-13 14:02:29 -0500

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InterestSignupComponent } from './interest-signup/interest-signup.component';
+import { ServerMaintenanceComponent } from './server-maintenance/server-maintenance.component';
 
 export const routes: Routes = [
     {path: 'interest', component: InterestSignupComponent},
+    {path: 'server-maintenance', component: ServerMaintenanceComponent},
     {path: '', redirectTo: '/interest', pathMatch: 'full'},
     {path: '**', redirectTo: '/interest'},
 ];
